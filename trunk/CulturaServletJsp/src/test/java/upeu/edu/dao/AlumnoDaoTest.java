519f94d0a75acb1767ee1ef5b9ba3b6472893fd6
mamanipari@gmail.com <mamanipari@gmail.com@01c6c4e7-6462-1b06-ed48-c0ef01032630>
2012-09-12 12:51:41 +0000

    Modificando el Pom 7 y Tests

--- trunk/CulturaServletJsp/src/test/java/upeu/edu/dao/AlumnoDaoTest.java
+++ trunk/CulturaServletJsp/src/test/java/upeu/edu/dao/AlumnoDaoTest.java
@@ -5,39 +5,32 @@
 package upeu.edu.dao;
 
 import java.util.List;
-import static org.junit.Assert.*;
-import org.junit.*;
+import junit.framework.TestCase;
 import upeu.edu.to.AlumnoTo;
 
 /**
  *
  * @author Intel
  */
-public class AlumnoDaoTest {
+public class AlumnoDaoTest extends TestCase {
     
-    public AlumnoDaoTest() {
-    }
-
-    @BeforeClass
-    public static void setUpClass() throws Exception {
-    }
-
-    @AfterClass
-    public static void tearDownClass() throws Exception {
+    public AlumnoDaoTest(String testName) {
+        super(testName);
     }
     
-    @Before
-    public void setUp() {
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
     }
     
-    @After
-    public void tearDown() {
+    @Override
+    protected void tearDown() throws Exception {
+        super.tearDown();
     }
 
     /**
      * Test of insertarAlumno method, of class AlumnoDao.
      */
-    @Test
     public void testInsertarAlumno() {
         System.out.println("insertarAlumno");
         AlumnoTo to = null;
@@ -46,81 +39,11 @@ public class AlumnoDaoTest {
         int result = instance.insertarAlumno(to);
         assertEquals(expResult, result);
         // TODO review the generated test code and remove the default call to fail.
-        //fail("The test case is a prototype.");
+       // fail("The test case is a prototype.");
     }
 
     /**
      * Test of buscarAlumnoId method, of class AlumnoDao.
      */
-  /*  @Test
-    public void testBuscarAlumnoId() {
-        System.out.println("buscarAlumnoId");
-        String idAlumno = "";
-        AlumnoDao instance = new AlumnoDao();
-        AlumnoTo expResult = null;
-        AlumnoTo result = instance.buscarAlumnoId(idAlumno);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-*/
-    /**
-     * Test of buscarDatos method, of class AlumnoDao.
-     */
-    /*
-    @Test
-    public void testBuscarDatos() {
-        System.out.println("buscarDatos");
-        String datos = "";
-        AlumnoDao instance = new AlumnoDao();
-        List expResult = null;
-        List result = instance.buscarDatos(datos);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-*/
-    /**
-     * Test of reporteAlumno method, of class AlumnoDao.
-     */
-    /*
-    @Test
-    public void testReporteAlumno() {
-        System.out.println("reporteAlumno");
-        AlumnoDao instance = new AlumnoDao();
-        List expResult = null;
-        List result = instance.reporteAlumno();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }*/
-
-    /**
-     * Test of eliminarRegistroAlumno method, of class AlumnoDao.
-     */
-    /*
-    @Test
-    public void testEliminarRegistroAlumno() {
-        System.out.println("eliminarRegistroAlumno");
-        int id_alumno = 0;
-        AlumnoDao instance = new AlumnoDao();
-        instance.eliminarRegistroAlumno(id_alumno);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }*/
-
-    /**
-     * Test of actualizarDatosAlumno method, of class AlumnoDao.
-     */
-    /*
-    @Test
-    public void testActualizarDatosAlumno() {
-        System.out.println("actualizarDatosAlumno");
-        AlumnoTo to = null;
-        int id_alumno = 0;
-        AlumnoDao instance = new AlumnoDao();
-        instance.actualizarDatosAlumno(to, id_alumno);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }*/
+   
 }
